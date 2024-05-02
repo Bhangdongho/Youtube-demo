47f4d960762865958e8457f831a59113be4f20f9
Bhangdongho <bhang97@naver.com>
2024-05-02 23:34:52 +0900

    get channels/:id

--- channel-demo.js
+++ channel-demo.js
@@ -12,13 +12,33 @@ app
     res.send('전체 조회');
   }) // 채널 전체 조회
   .post((req, res) => {
-    res.send('개별 생성');
-  }); // 채널 개별 생성
+    if (req.body.channelTitle) {
+      db.set(id++, req.body);
+
+      res.status(201).json({
+        message: `${db.get(id - 1).channelTitle}채널을 응원합니다.`,
+      });
+    } else {
+      res.status(400).json({
+        message: '요청 값을 제대로 보내주세요.',
+      });
+    }
+  }); // 채널 개별 생성 = db에 저장
 
 app
   .route('/channels/:id')
   .get((req, res) => {
-    res.send('개별 조회');
+    let { id } = req.params;
+    id = parseInt(id);
+
+    var channel = db.get(id);
+    if (channel) {
+      res.status(200).json(channel);
+    } else {
+      res.status(404).json({
+        message: '채널 정보를 찾을 수 없습니다.',
+      });
+    }
   }) // 채널 개별 조회
   .put((req, res) => {
     res.send('개별 수정');

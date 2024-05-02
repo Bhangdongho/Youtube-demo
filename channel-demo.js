03d2d4c5d278bccdc8d22895b389457f05eba14e
Bhangdongho <bhang97@naver.com>
2024-05-03 00:10:23 +0900

    get  channels

--- channel-demo.js
+++ channel-demo.js
@@ -9,7 +9,19 @@ var id = 1;
 app
   .route('/channels')
   .get((req, res) => {
-    res.send('전체 조회');
+    if (db.size) {
+      var channels = [];
+
+      db.forEach(function (value, key) {
+        channels.push(value);
+      });
+
+      res.status(200).json(channels);
+    } else {
+      res.status(404).json({
+        message: '조회할 채널이 없습니다.',
+      });
+    }
   }) // 채널 전체 조회
   .post((req, res) => {
     if (req.body.channelTitle) {

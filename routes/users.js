685d18f531824f9cc58a82393377c9aa7c77d3a5
Bhangdongho <bhang97@naver.com>
2024-05-10 03:18:28 +0900

    users.js 리팩토링

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,27 @@
-// express 모듈 셋팅
 const express = require('express')
 const router = express.Router()
-
 const conn = require('../mariadb')
 
-router.use(express.json()) // http 외 모듈 'json'
+router.use(express.json())
 
 // 로그인
 router.post('/login', function (req, res) {
-  // email이 디비에 저장된 회원인지 확인해야함
   const { email, password } = req.body
 
-  conn.query(
-    `SELECT * FROM users WHERE email = ?`,
-    email,
-    function (err, results) {
-      var loginUser = results[0]
-      if (results.length && loginUser.password == password) {
-        res.status(200).json({
-          message: `${loginUser.name}님 로그인 되었습니다.`
-        })
-      } else {
-        res.status(404).json({
-          message: '이메일 또는 비밀번호가 틀렸습니다.'
-        })
-      }
+  let sql = `SELECT * FROM users WHERE email = ?`
+  conn.query(sql, email, function (err, results) {
+    var loginUser = results[0]
+    // email이 디비에 저장된 회원인지 확인해야함
+    if (loginUser && loginUser.password == password) {
+      res.status(200).json({
+        message: `${loginUser.name}님 로그인 되었습니다.`
+      })
+    } else {
+      res.status(404).json({
+        message: '이메일 또는 비밀번호가 틀렸습니다.'
+      })
     }
-  )
+  })
 })
 
 // 회원가입
@@ -38,13 +33,11 @@ router.post('/join', function (req, res) {
   } else {
     const { email, name, password, contact } = req.body
 
-    conn.query(
-      `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`,
-      [email, name, password, contact],
-      function (err, results) {
-        res.status(201).json(results)
-      }
-    )
+    let sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`
+    let values = [email, name, password, contact]
+    conn.query(sql, values, function (err, results) {
+      res.status(201).json(results)
+    })
   }
 })
 
@@ -53,24 +46,18 @@ router
   .get(function (req, res) {
     let { email } = req.body
 
-    conn.query(
-      `SELECT * FROM users Where email = ?`,
-      email,
-      function (err, results) {
-        res.status(200).json(results)
-      }
-    )
+    let sql = `SELECT * FROM users WHERE email = ?`
+    conn.query(sql, email, function (err, results) {
+      res.status(200).json(results)
+    })
   })
   .delete(function (req, res) {
     let { email } = req.body
 
-    conn.query(
-      `DELETE FROM users Where email = ?`,
-      email,
-      function (err, results) {
-        res.status(200).json(results)
-      }
-    )
+    let sql = `DELETE FROM users Where email = ?`
+    conn.query(sql, email, function (err, results) {
+      res.status(200).json(results)
+    })
   })
 
 module.exports = router
